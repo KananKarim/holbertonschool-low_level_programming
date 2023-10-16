754b91e5440af9c35f4bfc8567e3833c0a21dec9
KananKarim <kenankerimli7770@gmail.com>
2023-10-16 06:36:28 -0700

    numbers

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n1;
+int n2;
+for (n1 = 0; n1 < 99; n1++)
+{
+for (n2 = n1 + 1; n2 < 100; n2++)
+{
+putchar((n1 / 10) + '0');
+putchar((n1 % 10) + '0');
+putchar(' ');
+putchar((n2 / 10) + '0');
+putchar((n2 % 10) + '0');
+if (n1 == 98 && n2 == 99)
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
