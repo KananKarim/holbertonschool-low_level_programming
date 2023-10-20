6863ce0dc00f3024a4ba220747648f73af6c6a89
KananKarim <kenankerimli7770@gmail.com>
2023-10-20 00:25:54 -0700

    diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_diagonal - prints a diagonal
+*@n:parameter
+*Return:returns nothing
+*/
+
+void print_diagonal(int n)
+{
+int i, space;
+if (n >= 0)
+{
+for (i = 0; i < n; i++)
+{
+for (space = 0; space < i; space++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
