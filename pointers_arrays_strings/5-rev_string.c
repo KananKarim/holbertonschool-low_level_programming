50376709ba72975bfab9e8989210a48c19d6a4c0
KananKarim <kenankerimli7770@gmail.com>
2023-10-23 03:15:06 -0700

    5 task

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *rev_string - function
+ *@s: string
+ *Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+int i, j;
+i = 0;
+
+while (s[i] != '\0')
+{
+i++;
+}
+
+for (j = i - 1; j >= 0; j--)
+{
+_putchar(s[j]);
+}
+
+_putchar('\n');
+}
