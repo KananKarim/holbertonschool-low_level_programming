44a2fe0b41a0877c89c83b290f19207867c41c4b
KananKarim <kenankerimli7770@gmail.com>
2023-10-26 07:18:23 -0700

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#define NULL ((void *)0)
+
+
+/**
+ *_strchr - func
+ * @s:string
+ * @c:which letter
+ * Return:firsr occurence
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (NULL);
+}
