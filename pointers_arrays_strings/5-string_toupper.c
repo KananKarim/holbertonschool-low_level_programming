03c6e073481e1248d6ba6b67e930a610f555c0bf
KananKarim <kenankerimli7770@gmail.com>
2023-10-26 04:02:07 -0700

    upper!

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_toupper - function
+ * @str : string
+ * Return: string
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 'a' + 'A';
+		}
+		i++;
+	}
+	return (str);
+}
