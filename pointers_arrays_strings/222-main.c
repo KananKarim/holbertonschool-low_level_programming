17bf8c3b5e681538807a013a832843b65b995def
KananKarim <kenankerimli7770@gmail.com>
2023-10-26 07:44:06 -0700

    whyy

--- pointers_arrays_strings/222-main.c
+++ pointers_arrays_strings/222-main.c
@@ -11,11 +11,8 @@ int main(void)
     char *s = "hello";
     char *f;
 
-    f = _strchr(s, 'l');
+    f = _strchr(s, 'a');
 
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
+    printf("%s\n", (f == NULL ? "nil" : f));
     return (0);
 }
