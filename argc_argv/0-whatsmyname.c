2ad9dc5fa4dfbcc1a6b1780a6efe88bbef8d763e
KananKarim <kenankerimli7770@gmail.com>
2023-11-01 08:02:05 -0700

    first task

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: the number of the arguments
+ * @argv: the array of arguments
+ * Return: true
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
