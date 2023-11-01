fcfe998ef0507e4cbdae7a65a6f2fe16aba1bb3f
KananKarim <kenankerimli7770@gmail.com>
2023-11-01 08:10:18 -0700

    second taks is done

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: the number of the arguments
+ * @argv: the array of arguments
+ * Return: true
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
