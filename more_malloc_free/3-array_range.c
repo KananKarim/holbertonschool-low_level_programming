5e80edd5b62d343a0f9980b35aeaa0b2bdd5b49a
KananKarim <kenankerimli7770@gmail.com>
2023-11-07 07:32:41 -0800

    end

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+/**
+ * array_range - function
+ * @min: min value
+ * @max: max value
+ * Return: array values between min max
+ */
+
+int *array_range(int min, int max)
+{
+	unsigned int i;
+
+	int *array;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	array = malloc(sizeof(char) * (max - min + 1));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; min <= max; i++, min++)
+	{
+		array[i] = min;
+	}
+	return (array);
+}
