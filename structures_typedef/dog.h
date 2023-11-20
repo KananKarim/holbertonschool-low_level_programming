e3aa076cbdbb44f67f11818b9e9282fb933ac467
KananKarim <kenankerimli7770@gmail.com>
2023-11-19 23:11:04 -0800

    betty leave!

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog{
+/**
+ * struct dog - struct combine data about poppy
+ *@name:name
+ *@age:age
+ *@owner:owner
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
