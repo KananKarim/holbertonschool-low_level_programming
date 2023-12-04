bd305e2d467052f967f53e460cbea01dd7b6a18c
KananKarim <kenankerimli7770@gmail.com>
2023-12-04 03:37:34 -0800

    third task added

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_node - Adds a new node to list_t list.
+ * @head: Double pointer to the head of the list.
+ * @str: String to be included in the new node.
+ *
+ * Return: Address of the new element, or NULL if it failed.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	size_t len = 0;
+	list_t *new_node;
+
+	while (str[len])
+		len++;
+
+	new_node = malloc(sizeof(list_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
