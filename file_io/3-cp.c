c90bd89999248b12d8e6baf83d859ea78224708b
KananKarim <kenankerimli7770@gmail.com>
2023-12-14 02:14:38 -0800

    copy file!

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,39 +3,55 @@
 #define BUFFER_SIZE 1024
 
 /**
- * main - copies the content of a file to another file
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success, exit with code on failure
+ * openReading - Opens a file for reading and handles errors.
+ * @filename: The name of the file to open.
+ *
+ * Return: File descriptor of the opened file.
  */
 
-int main(int argc, char *argv[])
+int openReading(const char *filename)
 {
-	int fd_from, fd_to, rd, wr, cl;
-	char buffer[BUFFER_SIZE];
+	int fd = open(filename, O_RDONLY);
 
-	if (argc != 3)
+	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	fd_from = open(argv[1], O_RDONLY);
-
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 		exit(98);
 	}
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	return (fd);
+}
 
-	if (fd_to == -1)
+/**
+ * openWriting - Opens a file for writing and handles errors.
+ * @filename: The name of the file to open.
+ *
+ * Return: File descriptor of the opened file.
+ */
+
+int openWriting(const char *filename)
+{
+	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 		exit(99);
 	}
+
+	return (fd);
+}
+
+/**
+ * copyFile - Copies data from one file descriptor to another.
+ * @fd_from: The source file descriptor.
+ * @fd_to: The destination file descriptor.
+ */
+
+void copyFile(int fd_from, int fd_to)
+{
+	char buffer[BUFFER_SIZE];
+	ssize_t rd, wr;
 
 	while ((rd = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
@@ -43,7 +59,7 @@ int main(int argc, char *argv[])
 
 		if (wr == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to file\n");
 			close(fd_from);
 			close(fd_to);
 			exit(99);
@@ -52,27 +68,55 @@ int main(int argc, char *argv[])
 
 	if (rd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file\n");
 		close(fd_from);
 		close(fd_to);
 		exit(98);
 	}
+}
 
-	cl = close(fd_from);
+/**
+ * closeFile - Closes a file descriptor and handles errors.
+ * @fd: The file descriptor to close.
+ * @filename: The name of the file associated with the file descriptor.
+ */
+
+void closeFile(int fd, const char *filename)
+{
+	int cl = close(fd);
 
 	if (cl == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd for %s\n", filename);
 		exit(100);
 	}
+}
 
-	cl = close(fd_to);
+/**
+ * main - Entry point of the program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ *
+ * Return: Always 0.
+ */
 
-	if (cl == -1)
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
+
+	fd_from = openReading(argv[1]);
+
+	fd_to = openWriting(argv[2]);
+
+	copyFile(fd_from, fd_to);
+	closeFile(fd_from, argv[1]);
+	closeFile(fd_to, argv[2]);
 
 	return (0);
 }
